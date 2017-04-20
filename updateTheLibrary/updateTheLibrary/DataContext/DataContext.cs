1620d3067320240a979eb1cc5093880ca8d99a72
Jeff Longo <jeffreylongo@yahoo.com>
2017-04-20 13:55:49 -0400

    made default connectionstring

---
+++ updateTheLibrary/updateTheLibrary/DataContext/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace updateTheLibrary.DataContext
+{
+    public class DataContext :DbContext
+    {
+        public DataContext():base("name=DefaultConnection")
+        {
+
+        }
+    }
+}
